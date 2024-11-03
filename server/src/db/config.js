bb306c351bab064639437311be9a0764534a1eb7
Suresh Kumar <sureshgehlot@7860gmail.com>
2024-11-03 16:40:41 +0530

    update

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const url = `mongodb+srv://${process.env.DATABASE_USER_NAME}:${process.env.DATABASE_USER_PASSWORD}.0qp461h.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&appName=${process.env.DATABASE_APP_NAME}`;
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 15000, // Increase timeout to 15 seconds
 })
 .then(() => {
