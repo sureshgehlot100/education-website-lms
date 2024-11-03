a57e56a1daebf4c83e9ed585f2c2144d76a5dd4c
Suresh Kumar <sureshgehlot@7860gmail.com>
2024-11-03 16:23:43 +0530

    vercel

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 
 const url = `mongodb+srv://${process.env.DATABASE_USER_NAME}:${process.env.DATABASE_USER_PASSWORD}.0qp461h.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&appName=${process.env.DATABASE_APP_NAME}`;
 
-mongoose.connect(url)
-    .then(() => {
-        console.log('databse connected successfully');
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 15000, // Increase timeout to 15 seconds
+})
+.then(() => {
+    console.log('database connected successfully');
+})
+.catch((error) => {
+    console.log('Database connection error:', error);
+});
