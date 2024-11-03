1902a59cca4fc8486a973325210c17e73a6fa4f4
Suresh Kumar <sureshgehlot@7860gmail.com>
2024-11-03 17:32:15 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(allRoutes);
 
 registerAdmin();
 
-// app.listen(process.env.PORT,()=>{
-//     console.log(`Server is running on port ${process.env.PORT}`)
-// });
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+});
 module.exports = app;
